5a43e9df9e06a4b7733a5e292c9ea761b9f70944
charlesscheuer <cscheuer@wustl.edu>
2021-12-16 16:25:14 -0800

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { motion } from "framer-motion";
 
+const x = 60;
+const y = 20;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-full w-full bg-bg">
+      {" "}
+      {[...Array(y)].map((e, i) => (
+        <div className="flex justify-around py-2">
+          {[...Array(x)].map((e, i) => (
+            <motion.div
+              animate={{
+                left: [20, 0, -20],
+                opacity: [0.3, 1, 0.1],
+              }}
+              transition={{
+                type: "spring",
+                repeat: Infinity,
+                duration: 4,
+                delay: i * 0.1,
+              }}
+              className="bg-dot relative"
+              style={{ height: 2, width: 2 }}
+              key={i}
+            />
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dot: "#393a3b", // Primary
+        bg: "#121315",
+      },
+    },
+  },
+  plugins: [],
+};
